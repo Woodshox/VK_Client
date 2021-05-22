e31c2a24e86754d7350f2d86c20a200abb8704ae
Woodshox <1aleksandrpavlov@gmail.com>
2021-05-22 13:45:10 +0200

    Create logo, button, Textfields. Prescribed the logic of the button and check for empty feilds.

--- VK_Client/ViewController.swift
+++ VK_Client/ViewController.swift
@@ -8,12 +8,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    
+    @IBOutlet weak var imageVK: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var loginLabel: UILabel!
+    @IBOutlet weak var passwordLabel: UILabel!
+    @IBOutlet weak var loginTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func pressButton(_ sender: Any) {
+        
+        if loginTextField.text == "admin",
+           passwordTextField.text == "123"{
+            print("OK")
+        }
+        else{
+            print("Error")
+        }
+    }
+    
+    
+    
+    
 }
 
