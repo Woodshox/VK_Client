fa1272cb5ddb939a07ef909ad30af1f0b207875d
Woodshox <1aleksandrpavlov@gmail.com>
2021-07-28 21:42:32 +0200

    Create function that moves the button to the visible area when the keyboard is appear

---
+++ VK_Client/File.swift
@@ -0,0 +1,23 @@
+//
+//  File.swift
+//  VK_Client
+//
+//  Created by Woodshox on 26.07.21.
+//
+
+import Foundation
+asdadsasdasd.asdadsasdasdas
+da
+sd
+a
+sd
+as
+datumaddsd
+a
+datumasdas
+NSMetadataUbiquitousItemHasUnresolvedConflictsKeya
+BSDa
+VDSUSPad
+Stringa
+
+asd
